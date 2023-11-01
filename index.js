ee5c798b79e1d6c00f94c812ba651cc0ded37efc
tarekul42 <tarekulrifat205@gmail.com>
2023-11-01 15:43:54 -0700

    update function added and complated this project

--- index.js
+++ index.js
@@ -32,11 +32,40 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/coffee/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
+      res.send(result)
+    })
+
     app.post('/coffee', async(req, res) =>{
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
+    })
+
+    app.put('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set:{
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier, 
+          taste: updatedCoffee.taste, 
+          category: updatedCoffee.category, 
+          details: updatedCoffee.details, 
+          photo: updatedCoffee.photo,
+        }
+      }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result)
+
     })
 
     app.delete('/coffee/:id', async(req, res) =>{
